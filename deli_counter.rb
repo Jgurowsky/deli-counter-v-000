e033c52a7c5a902242c7ce0ede789f2608db0fd8
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-04 09:03:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
-  end
   else
     katz_deli.each_with_index.collect do |name, index|
       "The line is currently: #{index + 1}. #{name}"
+    end
   end
 end
